e85cb444a34814f716c245c1cc7983dbd7d00ed2
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2024-06-15 20:24:49 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (72.32%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,18 +7,11 @@ public:
         while(mid<=high)
         {
             if(nums[mid] == 0)
-            {
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid] == 1)
-                mid++;
+                swap(nums[low++],nums[mid++]);
+            else if(nums[mid] == 2)
+                swap(nums[mid],nums[high--]);
             else
-            {
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+                mid++;
         }
         
     }
