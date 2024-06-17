f5bfb8b55f0c91d8ccca9c0b71f69a9e8177c5fe
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2024-06-17 16:02:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (60.92%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<n;j++)
+            for(int j=0;j<=i;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
