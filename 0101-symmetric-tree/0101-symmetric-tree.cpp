8091555d4625210bf487f703a6643b965770df78
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2024-07-01 00:35:51 +0530

    Time: 5 ms (34.72%), Space: 17.2 MB (72.59%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -14,10 +14,8 @@ public:
     
     bool helper(TreeNode * root1,TreeNode *root2)
     {
-        if( (root1 && !root2) || (!root1 && root2))
-        return false;
-        if( !root1 && !root2)
-        return true;
+        if( !root1 || !root2) //Better way to handle NULL cases
+        return root1==root2;
 
         return (root1->val == root2->val) && helper(root1->left,root2->right) && helper(root1->right,root2->left);
     }
