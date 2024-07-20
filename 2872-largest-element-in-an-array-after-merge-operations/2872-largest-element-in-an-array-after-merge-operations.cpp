2f40b06e0a1b48e0fa4691bfb87608cc987c10aa
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2024-07-20 18:46:58 +0530

    Time: 50 ms (99.38%), Space: 109.9 MB (40.68%) - LeetHub

--- 2872-largest-element-in-an-array-after-merge-operations/2872-largest-element-in-an-array-after-merge-operations.cpp
+++ 2872-largest-element-in-an-array-after-merge-operations/2872-largest-element-in-an-array-after-merge-operations.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     long long maxArrayValue(vector<int>& nums) {
-        
+         ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n=nums.size();
         long long ans=nums[n-1],sum=nums[n-1];
         for(int i=n-2;i>=0;i--)
