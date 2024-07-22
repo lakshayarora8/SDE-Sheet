8e59c7eda6fd28a2ce3046dec570fe3d3bce1a7a
lakshayarora8 <109210694+lakshayarora8@users.noreply.github.com>
2024-07-22 14:11:26 +0530

    Time: 3 ms (84.05%), Space: 11.7 MB (46.99%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -2,19 +2,29 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
 
-        set<int> unique(begin(nums),end(nums));
-        priority_queue<int,vector<int>,greater<int>> pq;
-        int maxi=INT_MIN;
-        for(auto it: unique)
+        int n=nums.size();
+        if(n<3)
+        return *max_element(begin(nums),end(nums));
+        long long first=LONG_MIN,second=LONG_MIN,third=LONG_MIN;
+
+        for(int i=0;i<n;i++)
         {
-            pq.push(it);
-            if(pq.size() >3)
-            pq.pop();
-            maxi=max(maxi,it);
+            if(nums[i] > first)
+            {
+                third=second;
+                second=first;
+                first=nums[i];
+            }
+            else if(nums[i] > third && nums[i]>second && nums[i]<first)
+            {
+                third=second;
+                second=nums[i];
+            }
+            else if(nums[i]>third && nums[i]<second)
+            {
+                third=nums[i];
+            }
         }
-        if(pq.size() == 3)
-        return pq.top(); 
-        else
-        return maxi;
+        return third==LONG_MIN ? first:third;
     }
 };
